18f87e5fac9d11727338fcb898313c997fca95c3
Shun Cheung <shunkongcheung@gmail.com>
2020-10-16 01:28:47 -0400

    feat(getSum): init

---
+++ getSum.js
@@ -0,0 +1,11 @@
+function getSum (a, b) {
+	const sum = a + b;
+	return sum
+}
+
+const firstResult = getSum(1, 2)
+const secondResult = getSum(3, 4)
+
+const thirdResult  = getSum(1, 2)
+
+console.log(`Results: ${firstResult} ${secondResult} ${thirdResult}`)
